99047a8f9bf42e90514a8e78143c749a7151eda9
CarlosDummy14 <carlosdummy14@gmail.com>
2020-06-16 00:31:21 -0500

    Server test

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const {config} = require('./config');
+
+const app = express();
+
+app.get('/', function (req, res){
+  res.send('hello world');
+})
+
+app.get('/json', function(req,res){
+  res.json({hello: 'world'})
+})
+
+app.listen(config.port, function(){
+  console.log(`Listening http://localhost:${config.port}`);
+})
